ca81c4ccbb16895109a074367eaa4fafdbe12cc5
hwangwoo <hwangwoo@hawaii.edu>
2019-04-09 20:43:55 -1000

    errors in master fixed

--- app/imports/ui/pages/TopPick.jsx
+++ app/imports/ui/pages/TopPick.jsx
@@ -5,7 +5,11 @@ export default class TopPick extends React.Component {
 
   render() {
     return (
-
+        <div className={'vendorMid'}>
+          <TopMenu/>
+          <TopPickMid/>
+          <BottomFooter/>
+        </div>
     );
   }
 }
--- app/imports/ui/pages/VendorHome.jsx
+++ app/imports/ui/pages/VendorHome.jsx
@@ -4,7 +4,11 @@ export default class VendorHome extends React.Component {
 
   render() {
     return (
-
+        <div className={'vendorMid'}>
+          <TopMenu/>
+          <VendorMid/>
+          <BottomFooter/>
+        </div>
     );
   }
 }
