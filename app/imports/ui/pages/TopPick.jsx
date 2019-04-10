9093583c91ce6eca913b5afcef5458c00c108c18
hwangwoo <hwangwoo@hawaii.edu>
2019-04-09 20:09:02 -1000

    VendorHome & TopTpick pages edit

--- app/imports/ui/pages/TopPick.jsx
+++ app/imports/ui/pages/TopPick.jsx
@@ -1,44 +1,21 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
-import { Vendors } from '/imports/api/vendor/vendor';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import '/client/style.css';
+import 'semantic-ui-css/semantic.min.css';
+import BottomFooter from '../components/BottomFooter';
+import TopMenu from '../components/TopMenu';
+import TopPickMid from '../components/TopPickMid';
 
-/** Renders a table containing all of the Vendor documents. */
-class TopPick extends React.Component {
+export default class TopPick extends React.Component {
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  /** Render the page once subscriptions have been received. */
-  renderPage() {
     return (
-        <Container>
-          <Header as="h2" textAlign="center" inverted>Today's Top Pick</Header>
-          <Card.Group>
-            {this.props.vendors.map((vendor, index) => <Vendors key={index}
-                                                               vendor={vendor}/>)}
-          </Card.Group>
-        </Container>
+        <div className={'vendorMid'}>
+          <TopMenu/>
+          <TopPickMid/>
+          <BottomFooter/>
+        </div>
     );
   }
 }
-
-/** Require an array of Vendor documents in the props. */
-TopPick.propTypes = {
-  vendors: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
-};
-
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
-  // Get access to Vendor documents.
-  const subscription = Meteor.subscribe('Vendors');
-  return {
-    vendors: Vendors.find({}).fetch(),
-    ready: subscription.ready(),
-  };
-})(TopPick);
+ReactDOM.render(<TopPick/>, document.getElementById('root'));
