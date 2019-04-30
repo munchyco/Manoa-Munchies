841ec39e82ee21e0ef6d5c380f35f227945cad67
Will Koch <wkoch530@gmail.com>
2019-04-29 15:21:47 -1000

    Update TopPick.jsx

--- app/imports/ui/pages/TopPick.jsx
+++ app/imports/ui/pages/TopPick.jsx
@@ -47,6 +47,7 @@ function shuffleArray(inputArray) {
     const j = Math.floor(Math.random() * (i + 1));
     [inputArray[i], inputArray[j]] = [inputArray[j], inputArray[i]];
   }
+  return inputArray;
 }
 
 /** Require an array of Food documents in the props. */
